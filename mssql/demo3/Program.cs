711a846171158d3cae084e4eb7b9a9afae5f15e9
dotnet111119 <57640157+dotnet111119@users.noreply.github.com>
2019-12-19 21:02:41 +0200

    Add files via upload

---
+++ mssql/demo3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSSQL1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["companydb_local"].ConnectionString))
+            {
+
+                SqlCommand cmd = new SqlCommand("SELECT_ALL_EMPLOYEES", conn);
+
+                cmd.Connection.Open();
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+
+                SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                List<Object> list = new List<object>();
+                while (reader.Read() == true)
+                {
+                    Console.WriteLine($" {reader["ID"]} {reader["NAME"]} {reader["AGE"]} {reader["ADDRESS"]} {reader["SALARY"]}");
+                    var e = new
+                    {
+                        Id = reader["ID"],
+                        firaName = reader["NAME"]
+                    };
+                    list.Add(e);
+                }
+            }
+
+            //cmd.Connection.Close();
+        }
+    }
+}
