c01587e86fe595ef49b48d1a1ae0b3757bd38713
dotnet111119 <57640157+dotnet111119@users.noreply.github.com>
2019-12-19 20:41:32 +0200

    Add files via upload

---
+++ mssql/demo1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSSQL1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["companydb_local"].
+                ConnectionString);
+            cmd.Connection.Open();
+            cmd.Connection.Close();
+        }
+    }
+}
